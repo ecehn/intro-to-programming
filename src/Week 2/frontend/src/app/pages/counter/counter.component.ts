e9ebf1fdc42d46907b5819f0012100908bd71bf6
Eric Chen <eric_chen@progressive.com>
2023-09-27 12:42:30 -0400

    counter before lunch

--- src/Week 2/frontend/src/app/pages/counter/counter.component.ts
+++ src/Week 2/frontend/src/app/pages/counter/counter.component.ts
@@ -3,11 +3,11 @@ import { Component } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { counterFeature } from "./state";
 import { CounterEvents } from "./state/counter.actions";
+import { PrefsComponent } from "./components/prefs/prefs.component";
 
 @Component({
   selector: "app-counter",
   standalone: true,
-  imports: [CommonModule],
   template: `
     <div>
       <button (click)="decrement()" class="btn btn-circle btn-sm">-</button>
@@ -15,14 +15,26 @@ import { CounterEvents } from "./state/counter.actions";
       <button (click)="increment()" class="btn btn-circle btn-sm">+</button>
     </div>
     <div>
-      <button (click)="reset()" class="btn btn-sm btn-accent">Reset</button>
+      <button
+        (click)="reset()"
+        [disabled]="resetDisabled()"
+        class="btn btn-sm btn-accent"
+      >
+        Reset
+      </button>
+    </div>
+    <div>
+      <app-prefs />
     </div>
   `,
   styles: [],
+  imports: [CommonModule, PrefsComponent],
 })
 export class CounterComponent {
   current = this.store.selectSignal(counterFeature.selectCurrent);
-
+  resetDisabled = this.store.selectSignal(
+    counterFeature.selectResetShouldBeDisabled,
+  );
   constructor(private readonly store: Store) {}
   increment() {
     this.store.dispatch(CounterEvents.incrementClicked());
